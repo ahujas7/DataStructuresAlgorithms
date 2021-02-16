32be5168caf041eb083fc5c1e8ce808b6a706a87
ahujas7 <ahuja.saksham@outlook.com>
2021-02-15 17:13:33 -0800

    GCD function

--- GCD.py
+++ GCD.py
@@ -1,18 +1,8 @@
+# Finding the greatest common denominator of two numbers
+
 def gcd(a, b):
-    if a > b:
-        higher = a
-        lower = b
-    else:
-        higher = b
-        lower = a
-    r = 0
-    while r != 0:
-        r = higher % lower
-        if r == 0:
-            return lower
-        else:
-            lower = r
-
-
-print(gcd(20, 8))
-
+    while b != 0:
+        temp = a
+        a = b
+        b = temp % a
+    return a
