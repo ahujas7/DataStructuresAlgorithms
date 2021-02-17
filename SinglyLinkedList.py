fb97dadd3121a7ceac2d9464089f0f31443294ae
ahujas7 <ahuja.saksham@outlook.com>
2021-02-16 20:32:09 -0800

    Started singly linked list

---
+++ SinglyLinkedList.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+        
+
+
